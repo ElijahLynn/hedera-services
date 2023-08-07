bcfb84e13616f1c093a771a08523f50fbcb09054
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-08-07 15:58:37 +0200

    Turn demo projects into independent builds
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- platform-sdk/platform-apps/tests/MigrationTestingTool/src/main/java/module-info.java
+++ platform-sdk/platform-apps/tests/MigrationTestingTool/src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module com.swirlds.demo.migration {
-    requires com.swirlds.platform.core;
-    requires com.swirlds.merkle;
+    requires com.swirlds.common;
     requires com.swirlds.fcqueue;
-    requires java.logging;
-    requires org.apache.logging.log4j;
-    requires lazysodium.java;
-    requires org.apache.commons.lang3;
-    requires com.swirlds.logging;
-    requires com.swirlds.virtualmap;
     requires com.swirlds.jasperdb;
+    requires com.swirlds.logging;
+    requires com.swirlds.merkle;
+    requires com.swirlds.platform.core;
+    requires com.swirlds.virtualmap;
+    requires java.logging;
+    requires org.apache.commons.lang3;
+    requires org.apache.logging.log4j;
 }
